285de032b5c29624898f0a458cf651f64639058a
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:48:01 -0600

    Create ejercicio2.py

---
+++ solucion de ejercicios/diccionarios/ejercicio2.py
@@ -0,0 +1,5 @@
+# Escribe un programa Python que cree un diccionario que contenga las notas de varios estudiantes y luego calcule el promedio de las notas.
+
+notas = {"Juan": 3.5, "María": 4.0, "Pedro": 2.8, "Ana": 4.5}
+promedio = sum(notas.values()) / len(notas)
+print("El promedio de notas es:", promedio)
