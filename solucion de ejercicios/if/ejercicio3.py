ff75eac72acfd5e83cee4e96d8663008a7d757ab
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:27:40 -0600

    Create ejercicio3.py

---
+++ solucion de ejercicios/if/ejercicio3.py
@@ -0,0 +1,7 @@
+# Escribe un programa Python que pida al usuario que ingrese un número y determine si es par o impar.
+
+num = int(input("Ingresa un número: "))
+if num % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
