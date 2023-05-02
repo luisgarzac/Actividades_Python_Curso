770d697022a1b8ba5d1061fd977566a87452e144
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:32:56 -0600

    Create ejercicio3.py

---
+++ solucion de ejercicios/arrays/ejercicio3.py
@@ -0,0 +1,4 @@
+# Escribe un programa Python que cree un arreglo (array) de números pares del 2 al 20 y luego imprima los elementos del arreglo en orden ascendente.
+
+numeros = list(range(2, 21, 2))
+print(numeros)
