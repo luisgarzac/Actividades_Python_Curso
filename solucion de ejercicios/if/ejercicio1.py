841a6bf969a473f6e014fa3f6b68d471a4dd1e8a
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:24:27 -0600

    Update ejercicio1.py

--- solucion de ejercicios/if/ejercicio1.py
+++ solucion de ejercicios/if/ejercicio1.py
@@ -1,1 +1,10 @@
+# En un programa Python, pide al usuario que ingrese un número. Si el número es positivo, imprime "El número es positivo". Si es negativo, 
+# imprime "El número es negativo". Si es cero, imprime "El número es cero".
 
+num = float(input("Ingresa un número: "))
+if num > 0:
+    print("El número es positivo")
+elif num < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
