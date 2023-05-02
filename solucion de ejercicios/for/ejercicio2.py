602f45de275ff39c4d3c5cae8c709a81fccdb2d8
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:28:59 -0600

    Create ejercicio2.py

---
+++ solucion de ejercicios/for/ejercicio2.py
@@ -0,0 +1,5 @@
+# Escribe un programa Python que pida al usuario una cadena de texto e imprima cada letra de la cadena en una línea separada.
+
+cadena = input("Ingresa una cadena de texto: ")
+for letra in cadena:
+    print(letra)
