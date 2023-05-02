12b9a21ce512ce835b6801b3d30b00bb0534d575
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:46:00 -0600

    Create ejercicio1.py

---
+++ solucion de ejercicios/scipy/ejercicio1.py
@@ -0,0 +1,7 @@
+# Escribe un programa Python que calcule la raíz cuadrada de un número utilizando la función sqrt de la librería scipy.
+
+from scipy import sqrt
+
+numero = 16
+raiz_cuadrada = sqrt(numero)
+print("La raíz cuadrada de", numero, "es:", raiz_cuadrada)
