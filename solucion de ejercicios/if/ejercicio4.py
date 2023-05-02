d1a7eba7ec07982d2737be07b1940024d955bcd6
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:28:05 -0600

    Create ejercicio4.py

---
+++ solucion de ejercicios/if/ejercicio4.py
@@ -0,0 +1,7 @@
+# Escribe un programa Python que pida al usuario su edad y determine si es mayor o menor de edad (mayor o igual a 18 años).
+
+edad = int(input("Ingresa tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
