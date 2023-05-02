d956513662a125de379e16b798ab22bbd2bb7f37
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:31:56 -0600

    Create ejercicio1.py

---
+++ solucion de ejercicios/arrays/ejercicio1.py
@@ -0,0 +1,5 @@
+# Escribe un programa Python que cree un arreglo (array) de números del 1 al 10 y luego imprima los elementos del arreglo en orden inverso.
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros.reverse()
+print(numeros)
