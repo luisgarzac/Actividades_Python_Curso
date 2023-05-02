e0c878d4ecb0caf41786d85864d9821f1e3fc238
luisgarzac <68243173+luisgarzac@users.noreply.github.com>
2023-05-02 14:47:44 -0600

    Create ejercicio1.py

---
+++ solucion de ejercicios/diccionarios/ejercicio1.py
@@ -0,0 +1,5 @@
+# Escribe un programa Python que cree un diccionario que contenga las edades de varios amigos y luego imprima la edad del amigo más joven.
+
+edades = {"Juan": 22, "María": 25, "Pedro": 21, "Ana": 24}
+edad_mas_joven = min(edades.values())
+print("La edad del amigo más joven es:", edad_mas_joven)
